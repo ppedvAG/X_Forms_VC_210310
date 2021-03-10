6b4220913e3a02044fb80a4d52e60f15c8ec0945
KlaasHagge <klaash@ppedv.de>
2021-03-10 16:23:41 +0100

    Add project files.

---
+++ X_Forms/X_Forms/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace X_Forms
+{
+    public partial class MainPage : ContentPage
+    {
+        //Property zum Zwischenspeichern der Personenliste (ObservableCollection ist eine generische Liste, welche die GUI
+        //automatisch über Veränderungen innerhalb der Liste informiert)
+        public ObservableCollection<Person> Personenliste { get; set; } = new ObservableCollection<Person>()
+        {
+            new Person(){Vorname="Anna", Nachname="Nass"}
+        };
+
+        //Konstruktor
+        public MainPage()
+        {
+            //Initialisierung der UI (Xaml-Datei). Sollte immer erste Aktion des Konstruktors sein
+            InitializeComponent();
+
+            //Neuzuweisung einer UI-Property über die x:Name-Property des Steuerelements
+            Lbl_Main.Text = "Neuer String";
+
+            //Zuweisung von EventHandlern zu den Completed-Events, damit ein besserer Bedienfluss gegeben ist
+            Ent_Vorname.Completed += (s, e) => Ent_Nachname.Focus();
+            Ent_Nachname.Completed += ImageButton_Clicked;
+
+            //Durch Setzen des BindingContextes nehmen Kurzbindungen aus dem XAML-Code automatisch Bezug auf die Properties
+            //des im BindingContext gesetzten Objekts
+            this.BindingContext = this;
+        }
+
+        //EventHandler
+        private void Btn_KlickMich_Clicked(object sender, EventArgs e)
+        {
+            //Neuzuweisung der Textproperty des Labeld mit dem Ausgewählten Element des Pickers
+            Lbl_Main.Text = Pkr_Monkeys.SelectedItem.ToString();
+
+            //Neuzuweisung einer Property des Eventauslösenden Steuerelements
+            (sender as Button).Text = "Geklicketer Button";
+        }
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            //Anzeige einer 'MessageBox' und Abfrage der User-Antwort
+            bool erg = await DisplayAlert("Begrüßung", $"Sei gegrüßt {Ent_Vorname.Text} {Ent_Nachname.Text}!\nSoll ein Eintrag erstellt werden?", "Ja", "Nein");
+
+            if (erg)
+                //Hinzufügen eines neuen Elements zur Liste (ObservableCollection informiert das ListView über Veränderung)
+                Personenliste.Add(new Person() { Vorname = Ent_Vorname.Text, Nachname = Ent_Nachname.Text });
+            else
+                await DisplayAlert("NEIN", "Schade auch", "Ok");
+        }
+
+        private void Btn_NameAendern_Clicked(object sender, EventArgs e)
+        {
+            //Änderung einer Property des BindingContexts des StackLayouts (INotifyPropertyChanged informiert GUI über Veränderung (vgl. Person.cs))
+            (Stl_DataBinding.BindingContext as Person).Nachname = "Müller";
+        }
+    }
+}
---
+++ X_Forms/X_Forms/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace X_Forms
+{
+    //Das Interface INotifyPropertyChanged informiert die GUI über Veränderungen in angebundenen .NET-Properties.
+    //Dazu muss das PropertyChanged-Event gefeuert werden, wenn die GUI auf eine Veränderung informieren soll (normalerweise innerhalb des Setters)
+    public class Person : INotifyPropertyChanged
+    {
+        public string Vorname { get; set; }
+
+        private string nachname;
+        public string Nachname 
+        { 
+            get => nachname; 
+            set
+            {
+                nachname = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Nachname)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
